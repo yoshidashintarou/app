19cd593da17d5a1e7d0f28dff3aea779d980a651
yoshidashintarou <y.shintaro.1999@gmail.com>
2022-08-17 08:53:27 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
    @books = Book.all
+   @book = Book.new
   end
 
   def new
@@ -13,11 +14,27 @@ class BooksController < ApplicationController
 
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     if  @book.save
      redirect_to book_path(@book.id)
     else
-      render :index
+     render :index
+    end
+  end
+
+  def edit
+   @book = Book.find(params[:id])
+
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
   end
 
@@ -25,5 +42,5 @@ end
 
 private
  def book_params
-  params.permit(:body, :title)
+    params.require(:book).permit(:title, :body)
  end
